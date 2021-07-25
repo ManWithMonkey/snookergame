2ecd6e5cf160e33afe9d6ac9d641c8b78333fe72
ManWithMonkey <ElixirOnOliver@protonmail.com>
2021-07-25 16:33:26 +0300

    keyboard input with ncurses test, weird new line behaviour

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <thread> // std::this_thread::sleep_for
+#include <ncurses.h>
 
 #define CONSOLE_WIDTH (140)
 #define CONSOLE_HEIGHT (52)
@@ -18,8 +19,23 @@
 #include "Game.h"
 #include "Window.h"
 
+int kbhit(void)
+{
+    int ch = getch();
+
+    if (ch != ERR) {
+        ungetch(ch);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main()
 {
+    initscr();
+    noecho();
+	
 	Console console(CONSOLE_WIDTH, CONSOLE_HEIGHT, " ---~:~--- [ BILLIARDS ] ---~:~--- ");
 	
 	ConsolePanel *gameWindow = new ConsolePanel(GAME_X, GAME_Y, GAME_W, GAME_H);
@@ -40,12 +56,17 @@ int main()
 		time_start = now;
 
 		double dt_seconds = (double)std::chrono::duration_cast<std::chrono::microseconds>(dt).count() / 1E+6;
-		// std::cout << dt_seconds << "\n";
+		
+		if(kbhit()){
+			auto c = getch();
+
+			std::cout << "this: " << c << "\n";
+		}
 
 		game.Update(dt_seconds);
 		game.UpdateScreen();
 
-		console.Render();
+		// console.Render();
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
