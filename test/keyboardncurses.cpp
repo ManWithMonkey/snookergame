9c390fc2817fcb03a72ad76dd9145dbf251671c7
DOLAN <abc@xyz.com>
2021-07-25 23:33:20 +0200

    fireworks

--- test/keyboardncurses.cpp
+++ test/keyboardncurses.cpp
@@ -1,37 +1,43 @@
 // from https://stackoverflow.com/questions/4025891/create-a-function-to-check-for-key-press-in-unix-using-ncurses
 
-#include <ncurses.h>
-#include <unistd.h>  /* only for sleep() */
+#include <curses.h>
+#include <unistd.h> /* only for sleep() */
 
 int kbhit(void)
 {
-    int ch = getch();
+	int ch = getch();
 
-    if (ch != ERR) {
-        ungetch(ch);
-        return 1;
-    } else {
-        return 0;
-    }
+	if (ch != ERR) {
+		ungetch(ch);
+		return 1;
+	} else {
+		return 0;
+	}
 }
+#include <locale.h>
 
 int main(void)
 {
-    initscr();
+	setlocale(LC_ALL, "C.UTF-8");
 
-    cbreak();
-    noecho();
-    nodelay(stdscr, TRUE);
+	initscr();
 
-    scrollok(stdscr, TRUE);
-    while (1) {
-        if (kbhit()) {
-            printw("Key pressed! It was: %d\n", getch());
-            refresh();
-        } else {
-            printw("No key pressed yet...\n");
-            refresh();
-            sleep(1);
-        }
-    }
+	cbreak();
+	noecho();
+	// nodelay(stdscr, TRUE);
+	halfdelay(8);
+	scrollok(stdscr, TRUE);
+
+	printw("|🍰|\n"); // unicode test
+
+	while (1) {
+		int ch = getch();
+		if (ch != ERR) {
+			printw("Key pressed! It was: %d\n", ch);
+		} else {
+			printw("No key pressed yet...\n");
+		}
+		refresh();
+
+	}
 }
