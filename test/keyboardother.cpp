25222ca031131b9b4a565e96f4b41e79e368e600
ManWithMonkey <ElixirOnOliver@protonmail.com>
2021-07-25 17:40:42 +0300

    keyboard test

---
+++ test/keyboardother.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+
+int kbhit()
+{
+    termios term;
+    tcgetattr(0, &term);
+
+    termios term2 = term;
+    term2.c_lflag &= ~ICANON;
+    tcsetattr(0, TCSANOW, &term2);
+
+    int byteswaiting;
+    ioctl(0, FIONREAD, &byteswaiting);
+
+    tcsetattr(0, TCSANOW, &term);
+
+    return byteswaiting > 0;
+}
+
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+int main()
+{
+    std::cout << "Press\n";
+	while (true) {
+		if(kbhit()){
+			char c = getch();
+			std::cout << "this: " << c << "\n";
+		}
+	}
+
+	return 0;
+}
