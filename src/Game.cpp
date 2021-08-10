fc4d515796b7f24bd2d1c8a4eb2b59b6962ebe19
ManWithMonkey <ElixirOnOliver@protonmail.com>
2021-08-10 10:35:38 +0300

    todo etc

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,10 +9,13 @@ Game::Game(){
 
     balls.push_back(ball);
 
-    // Line l1;
-    // l1.a = {2, 2};
-    // l1.b = {10, 2};
-    // lines.push_back(l1);
+    Line l1;
+    l1.a = {2, 2};
+    l1.b = {10, 2};
+    lines.push_back(l1);
+    lines.push_back(l1);
+    lines.push_back(l1);
+    lines.push_back(l1);
 
     lastUpdate = std::chrono::steady_clock::now();
 }
@@ -28,13 +31,19 @@ void Game::Update(){
         float velx = std::abs(ball.vel.x);
         float vely = std::abs(ball.vel.y);
 
-        if(x < ball.r)                              ball.vel.x = velx;
-        if(y < ball.r)                              ball.vel.y = vely;
-        if(x > w - 1.f - ball.r) ball.vel.x = -velx;
-        if(y > h - 1.f - ball.r) ball.vel.y = -vely;
+        // totally out of bounds
+        if(x < 0.f)     ball.vel.x = velx;
+        if(y < 0.f)     ball.vel.y = vely;
+        if(x > w - 1.f) ball.vel.x = -velx;
+        if(y > h - 1.f) ball.vel.y = -vely;
 
         ball.pos.x += ball.vel.x * deltaTime;
         ball.pos.y += ball.vel.y * deltaTime;
+
+        for(Line& line : lines){
+            // float distance = PointDistance(line.a, line.b, balls[0]);
+            // if(distance < )
+        }
     }
 
     float t = 2.f;
@@ -47,6 +56,11 @@ void Game::Update(){
     left    = {{l, t}, {l, b}};
     right   = {{r, t}, {r, b}};
 
+    lines[0] = top;
+    lines[1] = bottom;
+    lines[2] = left;
+    lines[3] = right;
+
     auto now = std::chrono::steady_clock::now();
     deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(now - lastUpdate).count() / 1000000.0f;
     lastUpdate = now;
@@ -57,14 +71,14 @@ void Game::Draw(){
         DrawBall(ball, 'O');
     }
 
-    // for(Line& line : lines){
-    //     DrawLine(line);
-    // }
+    for(Line& line : lines){
+        DrawLine(line, '#');
+    }
 
-    DrawLine(bottom, '#');
-    DrawLine(right, '#');
-    DrawLine(left, '#');
-    DrawLine(top, '#');
+    // DrawLine(bottom, '#');
+    // DrawLine(right, '#');
+    // DrawLine(left, '#');
+    // DrawLine(top, '#');
 }
 
 void Game::DrawBall(const Ball& ball, char c){
